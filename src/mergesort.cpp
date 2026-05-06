3ea1482332f593aa4517574d037a3a5449f1d6da
cortes1443 <jdcortesg2@eafit.edu.co>
2026-05-05 23:32:19 -0500

    mergeSort implementado a las solicitudes

---
+++ src/mergesort.cpp
@@ -0,0 +1,55 @@
+#include "mergesort.hpp"
+
+using namespace std;
+
+void merge(vector<Solicitud>& arr, int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    vector<Solicitud> L(n1);
+    vector<Solicitud> R(n2);
+
+    // Copiar datos
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[mid + 1 + j];
+
+    int i = 0, j = 0, k = left;
+
+    while (i < n1 && j < n2) {
+        if (L[i].tenure >= R[j].tenure) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    // Copiar sobrantes
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(vector<Solicitud>& arr, int left, int right) {
+    if (left >= right) return;
+
+    int mid = left + (right - left) / 2;
+
+    mergeSort(arr, left, mid);
+    mergeSort(arr, mid + 1, right);
+
+    merge(arr, left, mid, right);
+}
